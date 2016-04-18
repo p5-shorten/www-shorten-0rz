4c542d7364a6c8540d06f6100c97f849b55caf1f
Chase Whitener <cwhitener@gmail.com>
2016-04-18 18:17:03 -0400

    DZil-ify, scrape results with Mojo::DOM58

--- lib/WWW/Shorten/0rz.pm
+++ lib/WWW/Shorten/0rz.pm
@@ -1,38 +1,66 @@
 package WWW::Shorten::0rz;
 use strict;
 use warnings;
-use Carp;
-our $VERSION = '0.07';
+use Carp ();
+use Mojo::DOM58 ();
+use Try::Tiny qw(try catch);
+use WWW::Mechanize ();
+
 use base qw( WWW::Shorten::generic Exporter );
 our @EXPORT = qw( makeashorterlink makealongerlink );
 
-use WWW::Mechanize;
+our $_error_message = '';
+our $VERSION = '0.071';
+$VERSION = eval $VERSION;
 
 sub makeashorterlink {
-    my $url = shift;
-    my $ua = new WWW::Mechanize;
-    $ua->get('http://0rz.tw');
-    $ua->submit_form(fields => { url => $url });
-    if ($ua->response->is_success) {
-        my $short = $ua->uri;
-        if ($short =~qr{http://0rz.tw/.+=$}i) {
-            chop($short);
-            return $short;
-        }
+    my $url = shift or Carp::croak("No URL passed to makeashorterlink");
+    $_error_message = '';
+    return try {
+        my $mech = WWW::Mechanize->new;
+        $mech->add_header('Accept' => 'text/html');
+        $mech->get('http://0rz.tw/create');
+        $mech->submit_form(
+            form_id     => 'redirect-form',
+            fields      => { url => $url, },
+        );
+        return undef unless $mech->response->is_success;
+        my $dom = Mojo::DOM58->new($mech->response->decoded_content);
+        return $dom->find('div#doneurl > a')->last->attr('href');
     }
-    return undef;
+    catch {
+        $_error_message = $_;
+        return undef;
+    };
 }
 
 sub makealongerlink {
-    my $tinyurl_url = shift
-	or croak 'No TinyURL key / URL passed to makealongerlink';
+    my $url = shift or Carp::croak('No URL passed to makealongerlink');
+    $_error_message = '';
     my $ua = __PACKAGE__->ua();
-    $tinyurl_url = "http://0rz.tw/$tinyurl_url"
-        unless $tinyurl_url =~ m!^http://!i;
-    my $resp = $ua->get($tinyurl_url);
-    return undef unless $resp->is_redirect;
-    my $url = $resp->header('Location');
-    return $url;
+    $url = "http://0rz.tw/$url" unless $url =~ m!^http://!i;
+    my $resp = $ua->get($url);
+    unless ($resp->is_redirect) {
+        my $content = $resp->content;
+        if ($content =~ /Error/) {
+            if ($content =~ /<html/) {
+                $_error_message = 'Error is a html page';
+            }
+            elsif (length($content) > 100) {
+                $_error_message = substr($content, 0, 100);
+            }
+            else {
+                $_error_message = $content;
+            }
+        }
+        else {
+            $_error_message = 'Unknown error';
+        }
+
+        return undef;
+    }
+    my $long = $resp->header('Location');
+    return $long;
 }
 
 1;
@@ -41,36 +69,57 @@ __END__
 
 =head1 NAME
 
-WWW::Shorten::0rz - Shorten URL using 0rz.tw
+WWW::Shorten::0rz - Shorten URLs using L<http://0rz.tw/>
+
+=head1 SYNOPSIS
+
+  use strict;
+  use warnings;
+
+  use WWW::Shorten::0rz;
+  # use WWW::Shorten '0rz';  # or, this way
+
+  my $short_url = makeashorterlink('http://www.foo.com/some/long/url');
+  my $long_url  = makealongerlink($short_url);
 
 =head1 DESCRIPTION
 
-  use WWW::Shorten '0rz';
-  my $short_url = makeashorterlink($longurl);
+A Perl interface to the web site L<http://0rz.tw>.  The service simply maintains
+a database of long URLs, each of which has a unique identifier.
 
-=head1 METHODS
+=head1 FUNCTIONS
+
+=head2 makeashorterlink
+
+The function C<makeashorterlink> will call the L<http://orz.tw> web site passing
+it your long URL and will return the shorter version.
+
+=head2 makealongerlink
+
+The function C<makealongerlink> does the reverse. C<makealongerlink>
+will accept as an argument either the full URL or just the identifier.
+
+If anything goes wrong, then either function will return C<undef>.
+
+=head1 AUTHOR
+
+Kang-min Liu <F<gugod@gugod.org>>
+
+=head1 CONTRIBUTORS
 
 =over
 
-=item makeashorterlink( $url )
+=item *
 
-Given a very long $url, make it shorter using 0rz.tw url shorten
-service.
-
-=item makealongerlink( $url )
-
-Given a shorted $url, turn it to back to its longer version.
+Chase Whitener <F<capoeirab@cpan.org>>
 
 =back
 
-=head1 COPYRIGHT
+=head1 LICENSE AND COPYRIGHT
 
-Copyright 2004-2009 by Kang-min Liu <gugod@gugod.org>.
+Copyright 2004-2009 by Kang-min Liu <F<gugod@gugod.org>>.
 
 This program is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
 
-See <http://www.perl.com/perl/misc/Artistic.html>
-
 =cut
-
--- t/01.shorter.t
+++ t/01.shorter.t
@@ -1,15 +1,16 @@
-#!/usr/bin/perl
+use strict;
+use warnings;
 
-use Test::More qw(no_plan);
-
+use Test::More;
 use WWW::Shorten '0rz';
 
-my $short = makeashorterlink(
-   'http://www.appledaily.com.tw/template/twapple/index.cfm'
-   );
+my $URL = 'http://metacpan.org/pod/WWW::Shorten::0rz';
 
-is($short, 'http://0rz.tw/8204J');
+my $short = makeashorterlink( $URL );
 
-my $long = makealongerlink('http://0rz.tw/8204J');
-is($long,'http://www.appledaily.com.tw/template/twapple/index.cfm');
+is($short, 'http://0rz.tw/8Fc6i', 'shorten: got the right URL');
 
+my $long = makealongerlink('http://0rz.tw/8Fc6i');
+is($long, $URL, 'longer: got the original URL');
+
+done_testing();
